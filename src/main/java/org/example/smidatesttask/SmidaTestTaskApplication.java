db133df4d1f881ad5556db1b01b9f77a856d12b0
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-06-04 17:43:43 +0300

    initial commit

--- src/main/java/org/example/smidatesttask/SmidaTestTaskApplication.java
+++ src/main/java/org/example/smidatesttask/SmidaTestTaskApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SmidaTestTaskApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmidaTestTaskApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SmidaTestTaskApplication.class, args);
+    }
 
 }
