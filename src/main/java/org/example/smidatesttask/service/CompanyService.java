8f917ce4aa45ace6bd377a5a4aaf86b330b7ce47
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-06-05 18:21:19 +0300

    update company func

--- src/main/java/org/example/smidatesttask/service/CompanyService.java
+++ src/main/java/org/example/smidatesttask/service/CompanyService.java
@@ -10,6 +10,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.lang.reflect.Field;
+import java.sql.Timestamp;
+import java.time.Instant;
+
 /**
  * service for working with the logic of the company model
  */
@@ -29,6 +33,40 @@ public class CompanyService {
     @Transactional(isolation = Isolation.REPEATABLE_READ)
     @Retryable(maxAttempts = 5)
     public Company save(CompanyDTO companyDTO) {
+        var companyToSave = companyMapper.toCompany(companyDTO);
+        companyToSave.setCreatedAt(Timestamp.from(Instant.now()));
         return companyRepository.save(companyMapper.toCompany(companyDTO));
+    }
+
+    /**
+     * update company in db function
+     *
+     * @param companyDTO - company data from user
+     * @return the saved company
+     */
+    @Transactional(isolation = Isolation.REPEATABLE_READ)
+    @Retryable(maxAttempts = 5)
+    public Company update(CompanyDTO companyDTO) throws RuntimeException, IllegalAccessException {
+        Company companyToUpdate = companyRepository.findById(companyDTO.getId()).orElseThrow(
+                () -> new RuntimeException("Company with this id now found")
+        );
+
+        Field[] fields = companyDTO.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            Object value = field.get(companyDTO);
+            if (value != null && !field.getName().equals("createdAt")) {
+                Field companyField;
+                try {
+                    companyField = companyToUpdate.getClass().getDeclaredField(field.getName());
+                    companyField.setAccessible(true);
+                    companyField.set(companyToUpdate, value);
+                } catch (NoSuchFieldException e) {
+                    // ignore fields that are not found in the companyToUpdate object
+                }
+            }
+        }
+
+        return companyRepository.save(companyToUpdate);
     }
 }
