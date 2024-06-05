13f69e4165f9ac03ef344019872044e990b74f80
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-06-05 16:09:43 +0300

    create company repo test

---
+++ src/test/java/org/example/smidatesttask/repositoryTests/CompanyRepositoryTest.java
@@ -0,0 +1,68 @@
+package org.example.smidatesttask.repositoryTests;
+
+import org.example.smidatesttask.models.Company;
+import org.example.smidatesttask.repository.CompanyRepository;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * CompanyRepository tests
+ */
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@RunWith(SpringRunner.class)
+public class CompanyRepositoryTest {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    private Company testCompany;
+
+    /**
+     * set up the test company
+     */
+    @Before
+    public void setUp() {
+        testCompany = new Company();
+        testCompany.setAddress("test address");
+        testCompany.setName("test name");
+        testCompany.setRegistrationNumber("0123456789");
+        testCompany.setCreatedAt(Timestamp.from(Instant.now()));
+
+        testCompany = companyRepository.save(testCompany);
+    }
+
+    /**
+     * tear down the test company
+     */
+    @After
+    public void tearDown() {
+        if (companyRepository.findById(testCompany.getId()).isPresent()) {
+            companyRepository.delete(testCompany);
+        }
+    }
+
+    /**
+     * create test
+     */
+    @Test
+    public void company_whenSaved_thenCanBeFoundById() {
+        Company savedCompany = companyRepository.findById(testCompany.getId()).orElse(null);
+        assertNotNull(savedCompany);
+        assertEquals(testCompany, savedCompany);
+    }
+}
