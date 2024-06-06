767018ee8e15229486f61ec8680646325c0ebed4
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-06-06 14:18:35 +0300

    getReportDetails func

--- src/main/java/org/example/smidatesttask/service/ReportDetailsService.java
+++ src/main/java/org/example/smidatesttask/service/ReportDetailsService.java
@@ -2,9 +2,13 @@ package org.example.smidatesttask.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.smidatesttask.mapper.ReportDetailsMapper;
+import org.example.smidatesttask.model.ReportDetails;
 import org.example.smidatesttask.repository.ReportDetailsRepository;
 import org.example.smidatesttask.repository.ReportRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 /**
  * service for working with the logic of the report details model
@@ -17,4 +21,18 @@ public class ReportDetailsService {
     private final ReportDetailsRepository reportDetailsRepository;
     private final ReportRepository reportRepository;
     private final ReportDetailsMapper reportDetailsMapper;
+
+    /**
+     * get report details function
+     *
+     * @param reportId - report id
+     * @return the report details
+     * @throws RuntimeException if report was not found
+     */
+    @Transactional(readOnly = true)
+    public ReportDetails getReportDetails(UUID reportId) throws RuntimeException {
+        return reportDetailsRepository.findById(reportId).orElseThrow(
+                () -> new RuntimeException("Report details with this id not found")
+        );
+    }
 }
