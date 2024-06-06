448d786b92de33325a3e4828ce948948dc4178be
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-06-06 14:08:54 +0300

    init the report details service and report deatils dto and mapper

---
+++ src/main/java/org/example/smidatesttask/service/ReportDetailsService.java
@@ -0,0 +1,19 @@
+package org.example.smidatesttask.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.smidatesttask.mapper.ReportDetailsMapper;
+import org.example.smidatesttask.repository.ReportDetailsRepository;
+import org.example.smidatesttask.repository.ReportRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ * service for working with the logic of the report details model
+ */
+@Service
+@RequiredArgsConstructor
+public class ReportDetailsService {
+
+    private final ReportDetailsRepository reportDetailsRepository;
+    private final ReportRepository reportRepository;
+    private final ReportDetailsMapper reportDetailsMapper;
+}
