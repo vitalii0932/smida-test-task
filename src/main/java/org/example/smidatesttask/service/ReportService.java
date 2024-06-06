0e6b39d6ded7d269f64b21a71c5a92b3e81d4f22
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-06-06 12:00:08 +0300

    getAllReportsByCompany func

--- src/main/java/org/example/smidatesttask/service/ReportService.java
+++ src/main/java/org/example/smidatesttask/service/ReportService.java
@@ -2,8 +2,15 @@ package org.example.smidatesttask.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.smidatesttask.mapper.ReportMapper;
+import org.example.smidatesttask.model.Company;
+import org.example.smidatesttask.model.Report;
+import org.example.smidatesttask.repository.CompanyRepository;
 import org.example.smidatesttask.repository.ReportRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
 
 /**
  * service for working with the logic of the report model
@@ -14,5 +21,22 @@ public class ReportService {
 
     private final ValidationService validationService;
     private final ReportRepository reportRepository;
+    private final CompanyRepository companyRepository;
     private final ReportMapper reportMapper;
+
+    /**
+     * get all reports by some company function
+     *
+     * @param companyId - selected companyId
+     * @return a list of reports
+     * @throws RuntimeException if company was not found
+     */
+    @Transactional(readOnly = true)
+    public List<Report> getAllReportByCompany(UUID companyId) throws RuntimeException {
+        Company selectedCompany = companyRepository.findById(companyId).orElseThrow(
+                () -> new RuntimeException("The company with this id doesn't exist in the db")
+        );
+
+        return reportRepository.getAllByCompany(selectedCompany);
+    }
 }
