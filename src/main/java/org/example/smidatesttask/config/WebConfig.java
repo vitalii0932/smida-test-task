eed4fac135a904bb28ca8bb4e36c4d8136c5b37e
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-06-08 14:35:19 +0300

    WebConfig

---
+++ src/main/java/org/example/smidatesttask/config/WebConfig.java
@@ -0,0 +1,25 @@
+package org.example.smidatesttask.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * web configuration
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * loading static files for mvc templates
+     *
+     * @param registry - resource handler registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/");
+    }
+}
